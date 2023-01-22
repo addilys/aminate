d599726452bdd9f964549882b854e76e8cee6f9f
addilys <109492195+addilys@users.noreply.github.com>
2023-01-22 05:16:55 -0700

    Finished up the first real version!
    I would never have published the first commit publicly, this one I am a bit more confident in.
    
    - Renamed the call :update to :tick.
    - Renamed .imgd (image directory) to .is (image storage.)
    - Finished the :draw call.
    - Added control calls :start and :stop.
    - Fixed the .load function to actually include the image being used in the new table, and updated the table indexes in all functions to match.
    - Fixed the .load function to use the while loop properly (first time while loop user.)
    - .load's newQuad calls now use the image itself for dimension data instead of calling :getWidth and :getHeight for local sw and sh.
    - Tested with a sheet, all functionality confirmed WORKING.

--- aminate.lua
+++ aminate.lua
@@ -2,54 +2,63 @@
 -- whatre we aminatin
 --- thanks
 
-local amin = {}
+amin = {}
 amin.__index = amin
 amin.s = {}
-amin.imgd = {}
+amin.is = {}
 
 -- img, frame width, frame height, x gap, y gap, startframe, length, repeat, x start, y start
 function amin.image(directory, fw, fh, xg, yg)
-	amin.imgd[#amin.imgd + 1] = {love.graphics.newImage(directory), fw, fh, xg, yg}
-	return amin.imgd[#amin.imgd]
+	amin.is[#amin.is + 1] = {love.graphics.newImage(directory), fw, fh, xg, yg}
+	return amin.is[#amin.is]
 end
 function amin.load(image, xsf, ysf, xbl, xbr, ybb, l, spd, rpt)
-	local sw = image[1]:getWidth()
-	local sh = image[1]:getHeight()
-	local t = {false, spd, l, 0, 1, rpt}
+	local t = {image[1], false, spd, l, 0, 1, rpt}
 	local x = xsf
 	local y = ysf
-	while d = false do
-		t[#t+1] = love.graphics.newQuad(x*(image[2]+image[4]),y*(image[3]+image[5]), image[2], image[3], sw, sh)
+	local d = false
+	while(d == false) do
+		t[#t+1] = love.graphics.newQuad((x-1)*(image[2]+image[4]),(y-1)*(image[3]+image[5]), image[2], image[3], image[1])
 		x = x + 1
 		if x > xbr then 
 			x = xbl
 			y = y + 1
 			if y > ybb then d = true end
 		end 
-		if #t - 5 == l then d = true end
+		if #t - 7 == l then d = true end
 	end
 	amin.s[#amin.s+1] = setmetatable(t,amin)
 	return amin.s[#amin.s]
 end
 function amin.update(dt)
 	for n = 1, #amin.s, 1 do
-		if amin.s[n][1] == true then amin.s[n]:update(dt) end
+		if amin.s[n][2] == true then amin.s[n]:tick(dt) end
 	end
 end
-function amin:update(dt)
-	self[4] = self[4] + dt
-	if self[4] > self[2] then 
-		self[4] = self[4] - self[2]
-		self[5] = self[5] + 1
-		if self[5] > self[3] then 
-			if self[6] == true then self[5] = 1 end
-		else
-			self[1] = false
-			self[4] = 0
-			self[5] = 1
+function amin:tick(dt)
+	self[5] = self[5] + dt
+	if self[5] > self[3] then 
+		self[5] = self[5] - self[3]
+		self[6] = self[6] + 1
+		if self[6] > self[4] then 
+			if self[7] == true then 
+				self[6] = 1
+			else
+				self[2] = false
+				self[5] = 0
+				self[6] = 1
+			end
 		end
 	end
 end
+function amin:start()
+	self[2] = true
+end
+function amin:stop()
+	self[2] = false
+	self[5] = 0
+	self[6] = 1
+end
 function amin:draw(x, y)
-	
+	love.graphics.draw(self[1], self[7+self[6]], x, y)
 end
